bd732e06280ba075c3d8c322acae6d90db123885
Ebn Zhang <ebnbin@gmail.com>
2019-03-10 20:44:04 +0800

    Create DebugUtils.kt

---
+++ eb/src/main/java/com/ebnbin/eb/debug/DebugUtils.kt
@@ -0,0 +1,33 @@
+package com.ebnbin.eb.debug
+
+import android.util.Log
+import com.ebnbin.eb.BuildConfig
+
+/**
+ * Debug 模式.
+ */
+val debug: Boolean
+    get() = BuildConfig.DEBUG
+
+/**
+ * 在 debug 模式下输出日志.
+ *
+ * @param any 要输出的对象. 如果为 [Throwable] 则输出异常栈.
+ *
+ * @param priority 日志级别.
+ */
+fun log(vararg any: Any?, priority: Int = Log.ERROR, tag: String = "ebnbin") {
+    if (!debug) return
+    if (any.isEmpty()) return
+    val sb = StringBuilder()
+    any.forEach {
+        sb.appendln(if (it is Throwable) {
+            Log.getStackTraceString(it)
+        } else {
+            it
+        })
+    }
+    sb.chunked(4000).forEach {
+        Log.println(priority, tag, it)
+    }
+}
