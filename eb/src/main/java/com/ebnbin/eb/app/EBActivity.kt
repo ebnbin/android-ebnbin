4757d12292e2b64154d9ecb6138dab7aff0489b8
Ebn Zhang <ebnbin@gmail.com>
2019-03-10 23:38:33 +0800

    Create DebugSwipeDetector

--- eb/src/main/java/com/ebnbin/eb/app/EBActivity.kt
+++ eb/src/main/java/com/ebnbin/eb/app/EBActivity.kt
@@ -1,8 +1,10 @@
 package com.ebnbin.eb.app
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.ebnbin.eb.R
+import com.ebnbin.eb.debug.DebugSwipeDetector
 
 /**
  * Base Activity.
@@ -11,5 +13,14 @@ abstract class EBActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_AppCompat)
+    }
+
+    //*****************************************************************************************************************
+
+    private val debugSwipeDetector: DebugSwipeDetector = DebugSwipeDetector()
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (debugSwipeDetector.dispatchTouchEvent(ev)) return true
+        return super.dispatchTouchEvent(ev)
     }
 }
---
+++ eb/src/main/java/com/ebnbin/eb/debug/DebugSwipeDetector.kt
@@ -0,0 +1,76 @@
+package com.ebnbin.eb.debug
+
+import android.view.MotionEvent
+import com.ebnbin.eb.util.dpToPx
+
+/**
+ * 用于 debug 的多指滑动检测器.
+ */
+internal class DebugSwipeDetector {
+    /**
+     * 滑动距离.
+     */
+    private val offset: Float = 64f.dpToPx
+
+    /**
+     * 当指定数量的手指按下时设置为 true. 当最后一个手指抬起时设置为 false.
+     */
+    private var isSwiping: Boolean = false
+
+    /**
+     * 当检测到一次有效滑动或手指数量发生变化时设置为 true. 当最后一个手指抬起时设置为 false.
+     */
+    private var isSwiped: Boolean = false
+
+    private lateinit var downXYs: Array<Pair<Float, Float>>
+
+    /**
+     * @param motionEvent 可能被修改.
+     */
+    fun dispatchTouchEvent(motionEvent: MotionEvent?): Boolean {
+        motionEvent ?: return false
+        if (!debug) return false
+        val pointerCount = motionEvent.pointerCount
+        if (pointerCount == 3) {
+            if (!isSwiping) {
+                isSwiping = true
+                downXYs = Array(pointerCount) {
+                    Pair(motionEvent.getX(it), motionEvent.getY(it))
+                }
+                // 发送取消事件.
+                motionEvent.action = MotionEvent.ACTION_CANCEL
+                return false
+            }
+            if (isSwiped) return false
+            var leftToRight = true
+            var rightToLeft = true
+            var topToBottom = true
+            var bottomToTop = true
+            downXYs.forEachIndexed { index, (downX, downY) ->
+                val offsetX = motionEvent.getX(index) - downX
+                val offsetY = motionEvent.getY(index) - downY
+                if (offsetX < offset) leftToRight = false
+                if (offsetX > -offset) rightToLeft = false
+                if (offsetY < offset) topToBottom = false
+                if (offsetY > -offset) bottomToTop = false
+            }
+            when {
+                leftToRight -> log("leftToRight")
+                rightToLeft -> log("rightToLeft")
+                topToBottom -> log("topToBottom")
+                bottomToTop -> log("bottomToTop")
+                else -> return false
+            }
+            isSwiped = true
+        } else {
+            if (!isSwiping) return false
+            if (motionEvent.actionMasked == MotionEvent.ACTION_UP) {
+                isSwiping = false
+                isSwiped = false
+            } else if (!isSwiped) {
+                isSwiped = true
+            }
+        }
+        return false
+    }
+}
