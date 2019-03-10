b2a7d13ae5412231862d6eda37dea9a9b606d198
Ebn Zhang <ebnbin@gmail.com>
2019-03-10 21:23:32 +0800

    Add dpToPx utils

--- eb/src/main/java/com/ebnbin/eb/util/AppUtils.kt
+++ eb/src/main/java/com/ebnbin/eb/util/AppUtils.kt
@@ -1,9 +1,21 @@
 package com.ebnbin.eb.util
 
 import com.ebnbin.eb.app.EBApplication
+import kotlin.math.roundToInt
 
 /**
  * EBApplication 单例.
  */
 val ebApp: EBApplication
     get() = EBApplication.instance
+
+//*********************************************************************************************************************
+
+val Float.dpToPx: Float
+    get() = this * ebApp.resources.displayMetrics.density
+
+val Float.dpToPxRound: Int
+    get() = dpToPx.roundToInt()
+
+val Float.dpToPxInt: Int
+    get() = dpToPx.toInt()
