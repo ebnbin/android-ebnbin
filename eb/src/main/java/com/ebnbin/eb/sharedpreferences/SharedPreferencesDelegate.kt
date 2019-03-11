95d4c3c271513154110ae556d110f3ed18b5a417
Ebn Zhang <ebnbin@gmail.com>
2019-03-11 22:28:36 +0800

    Create SharedPreferencesDelegate

---
+++ eb/src/main/java/com/ebnbin/eb/sharedpreferences/SharedPreferencesDelegate.kt
@@ -0,0 +1,34 @@
+package com.ebnbin.eb.sharedpreferences
+
+import android.content.SharedPreferences
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * 偏好代理.
+ */
+open class SharedPreferencesDelegate<T>(
+    private val key: String,
+    private val defValue: T,
+    /**
+     * 获取偏好.
+     */
+    private val getSharedPreferences: () -> SharedPreferences = {
+        com.ebnbin.eb.sharedpreferences.getSharedPreferences()
+    },
+    /**
+     * 如果不为空则读取旧的值用于拦截偏好设置. 返回 true 则拦截偏好设置.
+     */
+    private val onSetValue: ((oldValue: T, newValue: T) -> Boolean)? = null
+) : ReadWriteProperty<Any?, T> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return getSharedPreferences().get(key, defValue)
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        val sharedPreferences = getSharedPreferences()
+        if (onSetValue?.invoke(sharedPreferences.get(key, defValue), value) != true) {
+            sharedPreferences.put(key, value)
+        }
+    }
+}
