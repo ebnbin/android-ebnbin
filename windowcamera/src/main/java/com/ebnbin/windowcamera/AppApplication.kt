7db9df97ddd39b9e0e79ad11f947e7a95ddad769
Ebn Zhang <ebnbin@gmail.com>
2019-05-27 16:58:03 +0800

    Fix video bug

--- windowcamera/src/main/java/com/ebnbin/windowcamera/AppApplication.kt
+++ windowcamera/src/main/java/com/ebnbin/windowcamera/AppApplication.kt
@@ -3,7 +3,14 @@ package com.ebnbin.windowcamera
 import com.ebnbin.eb.EBApplication
 import com.ebnbin.eb.debug.BaseDebugPageFragment
 import com.ebnbin.windowcamera.debug.DebugPageFragment
+import com.shuyu.gsyvideoplayer.player.PlayerFactory
+import tv.danmaku.ijk.media.exo2.Exo2PlayerManager
 
 class AppApplication : EBApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        PlayerFactory.setPlayManager(Exo2PlayerManager::class.java)
+    }
+
     override val debugPageFragmentClass: Class<out BaseDebugPageFragment>? = DebugPageFragment::class.java
 }
