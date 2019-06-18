8c275c5428de5f0cc03c057adfd86b58907591ac
Ebn Zhang <ebnbin@gmail.com>
2019-06-18 17:34:43 +0800

    Update path

--- windowcamera/src/main/java/com/ebnbin/windowcamera/util/IOHelper.kt
+++ windowcamera/src/main/java/com/ebnbin/windowcamera/util/IOHelper.kt
@@ -1,22 +1,21 @@
 package com.ebnbin.windowcamera.util
 
 import android.os.Environment
-import com.ebnbin.eb.util.BuildHelper
 import com.ebnbin.eb.util.TimeHelper
+import com.ebnbin.eb.util.ebApp
 import java.io.File
 
 object IOHelper {
-    fun getPath(): File {
-        val result = File(Environment.getExternalStorageDirectory(),
-            "${BuildHelper.applicationId}/${Environment.DIRECTORY_DCIM}/")
+    fun getPath(): File? {
+        val result = ebApp.getExternalFilesDir(Environment.DIRECTORY_DCIM) ?: return null
         if (!result.exists()) {
             result.mkdirs()
         }
         return result
     }
 
-    fun nextFile(extension: String): File {
-        val path = getPath()
+    fun nextFile(extension: String): File? {
+        val path = getPath() ?: return null
         val fileName = "${TimeHelper.string("yyyy_MM_dd_HH_mm_ss_SSS")}$extension"
         return File(path, fileName)
     }
@@ -26,15 +25,16 @@ object IOHelper {
 
     fun refreshFiles() {
         files = getPath()
-            .listFiles { _, name ->
+            ?.listFiles { _, name ->
                 when (name.substringAfterLast(".")) {
                     "jpg", "mp4", "3gp" -> true
                     else -> false
                 }
             }
-            .toSortedSet(Comparator { file1, file2 ->
+            ?.toSortedSet(Comparator { file1, file2 ->
                 compareValuesBy(file2, file1, File::lastModified, File::getName)
             })
-            .toList()
+            ?.toList()
+            ?: ArrayList()
     }
 }
