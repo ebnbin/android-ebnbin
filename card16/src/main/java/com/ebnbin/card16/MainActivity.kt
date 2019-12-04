f157b9770511f664a9959112a2e14fad13e93f5d
Ebn Zhang <ebnbin@gmail.com>
2019-12-04 17:33:52 +0800

    Update

--- card16/src/main/java/com/ebnbin/card16/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ebnbin.card16
-
-import com.ebnbin.eb.activity.EBActivity
-
-class MainActivity : EBActivity()
--- wanandroid/src/main/java/com/ebnbin/wanandroid/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ebnbin.wanandroid
-
-import com.ebnbin.eb.activity.EBActivity
-
-class MainActivity : EBActivity()
