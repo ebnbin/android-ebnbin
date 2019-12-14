55ae2b0d585eba377f69c4232d4cc0ede31a3fb8
Ebn Zhang <ebnbin@gmail.com>
2019-12-14 14:27:31 +0800

    Update

--- sample/src/main/java/com/ebnbin/sample/MainActivity.kt
+++ sample/src/main/java/com/ebnbin/sample/MainActivity.kt
@@ -1,18 +1,5 @@
 package com.ebnbin.sample
 
-import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.Observer
 import com.ebnbin.eb.activity.EBActivity
-import com.ebnbin.eb.debug.DebugHelper
-import com.jeremyliao.liveeventbus.LiveEventBus
 
-class MainActivity : EBActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        LiveEventBus.get(DebugHelper.KEY_LEFT_TO_RIGHT)
-            .observe(this, Observer {
-                Toast.makeText(this, "debug", Toast.LENGTH_SHORT).show()
-            })
-    }
-}
+class MainActivity : EBActivity()
