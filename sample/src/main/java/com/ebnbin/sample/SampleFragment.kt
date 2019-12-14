732b5a9d8908e96138d991a89b7019a592644d37
Ebn Zhang <ebnbin@gmail.com>
2019-12-15 04:40:03 +0800

    Update

---
+++ sample/src/main/java/com/ebnbin/sample/SampleFragment.kt
@@ -0,0 +1,8 @@
+package com.ebnbin.sample
+
+import com.ebnbin.eb.fragment.EBViewFragment
+import com.ebnbin.sample.databinding.SampleFragmentBinding
+
+class SampleFragment : EBViewFragment<SampleFragmentBinding>() {
+    override val layoutId: Int = R.layout.sample_fragment
+}
