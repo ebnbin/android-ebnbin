ef5316d90bafe196504782c9220d6f54eeb1986c
Ebn Zhang <ebnbin@gmail.com>
2019-12-23 02:44:56 +0800

    BundleExtensions.kt

---
+++ eb/src/main/java/com/ebnbin/eb/util/BundleExtensions.kt
@@ -0,0 +1,19 @@
+package com.ebnbin.eb.util
+
+import android.os.Bundle
+
+/**
+ * 返回 value 或 null.
+ */
+inline fun <reified T> Bundle.getValue(key: String): T? = get(key) as T?
+
+/**
+ * 返回 value 或抛出 [NullPointerException].
+ */
+inline fun <reified T> Bundle.requireValue(key: String): T = get(key) as T
+
+/**
+ * 如果 key 不存在则返回 [defaultValue], 否则返回 value 或抛出 [NullPointerException].
+ */
+inline fun <reified T> Bundle.getValueOrDefault(key: String, defaultValue: T): T =
+    if (containsKey(key)) requireValue(key) else defaultValue
