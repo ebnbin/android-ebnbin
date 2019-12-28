c22665b08deac524792ba02a2ad6d15f38e3779b
Ebn Zhang <ebnbin@gmail.com>
2019-12-28 14:57:08 +0800

    Update

--- eb-dev/src/main/java/com/ebnbin/eb/dev/DevPreferenceFragment.kt
+++ eb-dev/src/main/java/com/ebnbin/eb/dev/DevPreferenceFragment.kt
@@ -3,11 +3,20 @@ package com.ebnbin.eb.dev
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.ebnbin.eb.preference.Preference
+import com.ebnbin.eb.preference.PreferenceCategory
 
 internal class DevPreferenceFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         preferenceManager.sharedPreferencesName = DevSpManager.name
         preferenceScreen = preferenceManager.createPreferenceScreen(requireContext())
+
+        EBDev.addDevItems?.invoke(this)
+
+        PreferenceCategory(requireContext()).also {
+            preferenceScreen.addPreference(it)
+            it.title = "EB"
+            it.isIconSpaceReserved = false
+        }
 
         Preference(requireContext()).also {
             preferenceScreen.addPreference(it)
--- eb-dev/src/main/java/com/ebnbin/eb/dev/EBDev.kt
+++ eb-dev/src/main/java/com/ebnbin/eb/dev/EBDev.kt
@@ -1,13 +1,18 @@
 package com.ebnbin.eb.dev
 
 import android.app.Application
+import androidx.preference.PreferenceFragmentCompat
 
 object EBDev {
     internal lateinit var app: Application
         private set
 
-    fun init(app: Application) {
+    internal var addDevItems: ((PreferenceFragmentCompat) -> Unit)? = null
+
+    fun init(app: Application, addDevItems: ((PreferenceFragmentCompat) -> Unit)? = null) {
         this.app = app
+        this.addDevItems = addDevItems
+
         this.app.registerActivityLifecycleCallbacks(DevFloatingActivityLifecycleCallbacks)
     }
 }
