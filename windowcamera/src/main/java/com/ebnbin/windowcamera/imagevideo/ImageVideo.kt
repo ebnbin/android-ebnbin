c57ef5230cdc3996331a116f4d4e19b01723b87a
Ebn Zhang <ebnbin@gmail.com>
2020-01-05 14:57:29 +0800

    Update

--- windowcamera/src/main/java/com/ebnbin/windowcamera/imagevideo/ImageVideo.kt
+++
@@ -1,9 +0,0 @@
-package com.ebnbin.windowcamera.imagevideo
-
-import android.net.Uri
-import android.os.Parcelable
-import com.ebnbin.windowcamera.viewer.ViewerType
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-open class ImageVideo(val type: ViewerType, val uri: Uri, val index: Int): Parcelable
